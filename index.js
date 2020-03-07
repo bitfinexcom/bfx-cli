4395c371d4280366fea4ade8ff74e012f0319614
Cris Mihalache <me@f3rno.com>
2020-03-07 17:05:08 +0700

    (feature) credential storage/loading, with optional encryption

--- index.js
+++ index.js
@@ -6,17 +6,44 @@ if (!process.env.DEBUG) {
 }
 
 require('./lib/override_bfx_api_node_debug')
-
 require('pipe-args').load()
+
+const _isEmpty = require('lodash/isEmpty')
+const debug = require('debug')('>')
 const yArgs = require('yargs')
+
 const commands = require('./lib/commands')
+const loadConfig = require('./lib/load_config')
 
-const argv = yArgs
-  .scriptName('bfx-cli')
-  .usage('Usage: bfx-cli [command] <options>')
-  .demandCommand(1, 'A command is required')
-  .help()
+const run = async () => {
+  const argv = yArgs
+    .scriptName('bfx-cli')
+    .usage('Usage: bfx-cli [command] <options>')
+    .demandCommand(1, 'A command is required')
+    .middleware(async () => {
+      const config = await loadConfig()
+      const { key, secret } = config
 
-commands.forEach(def => argv.command(def))
+      if (_isEmpty(key) || _isEmpty(secret)) {
+        console.log('No API credentials set; run \'bfx-cli credentials\' to set them')
+        console.log('')
+        return
+      }
 
-argv.parse()
+      // setup on env for bitfinex-api-node scripts
+      process.env.API_KEY = key
+      process.env.API_SECRET = secret
+      process.env.BFX_CLI_CREDENTIALS_LOADED = true
+    })
+    .help()
+
+  commands.forEach(def => argv.command(def))
+
+  argv.parse()
+}
+
+try {
+  run()
+} catch (e) {
+  debug('%s', e.message)
+}
