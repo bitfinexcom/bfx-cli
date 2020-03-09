5fef0a374251911d129fc4e9da3f8fa2644a2fee
Cris Mihalache <me@f3rno.com>
2020-03-09 17:40:44 +0700

    (feature) add affiliate code command, rename salt file to settings

--- scripts/gen_salt.js
+++ scripts/gen_salt.js
@@ -2,31 +2,40 @@
 
 const fs = require('fs')
 
-const SALT_FN = './.salt.json'
+const SETTINGS_FN = './.settings.json'
 const SALT_LENGTH = 32
 const SALT_SRC = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-let saltExists = false
-
-try {
-  fs.statSync(SALT_FN)
-  saltExists = true
-} catch {
-  saltExists = false
-}
-
-if (!saltExists) {
+const generate = () => {
   let salt = ''
-
-  for (let i = 0; i < SALT_LENGTH; i += 1) {
-    salt += SALT_SRC[Math.floor(SALT_SRC.length * Math.random())]
-  }
+  let saltExists = false
 
   try {
-    fs.writeFileSync(SALT_FN, JSON.stringify({ salt }))
-  } catch (e) {
-    console.error('failed to write salt: %s', e.message)
+    fs.statSync(SETTINGS_FN)
+    saltExists = true
+  } catch (e) { // eslint-disable-line
+    saltExists = false
+  }
+
+  if (!saltExists) {
+    for (let i = 0; i < SALT_LENGTH; i += 1) {
+      salt += SALT_SRC[Math.floor(SALT_SRC.length * Math.random())]
+    }
+
+    try {
+      fs.writeFileSync(SETTINGS_FN, JSON.stringify({
+        affiliateCode: 'xZvWHMNR',
+        salt
+      }))
+    } catch (e) {
+      console.error('failed to write salt: %s', e.message)
+    }
   }
 }
 
-module.exports = SALT_FN
+generate()
+
+module.exports = {
+  settingsFilename: SETTINGS_FN,
+  generate
+}
